0ac863a95638d01c6a542536b9c899116a6970a3
Greatkingcj <331762692@qq.com>
2016-07-29 00:41:19 +0800

    Initial commit

---
+++ app/src/main/java/com/scu/charles/zoomimageview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.scu.charles.zoomimageview;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private int[] mImgs = new int[]{
+            R.drawable.test1,R.drawable.test2,R.drawable.test3,R.drawable.test4
+    };
+
+    private ImageView[] mImageViews = new ImageView[mImgs.length];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        viewPager.setAdapter(new PagerAdapter() {
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ZoomImageView imageView = new ZoomImageView(getApplicationContext());
+                imageView.setImageResource(mImgs[position]);
+                container.addView(imageView);
+                mImageViews[position] = imageView;
+                return imageView;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position,
+                                    Object object) {
+                container.removeView(mImageViews[position]);
+            }
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0==arg1;
+            }
+
+            @Override
+            public int getCount() {
+                return mImageViews.length;
+            }
+        });
+    }
+}
